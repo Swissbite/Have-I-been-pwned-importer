08449fd8ca5752302bca39b716ced0aa2682da1c
David Däster <david@daester.net>
2024-04-16 23:37:22 +0200

    Extract importer logic to importByRecord, fix licence

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,22 @@
-/**
- *      This file is part of "Have I been pwned - importer".
+/*
+ * Copyright (c) 2024 David Däster
  *
- *     "Have I been pwned - importer" is free software: you can redistribute
- *     it and/or modify it under the terms of the GNU General Public License
- *     as published by the Free  Software Foundation, either version 3 of the
- *     License, or (at your option) any later version.
+ * This file is part of "Have I been pwned - importer".
  *
- *     "Have I been pwned - importer" is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- *     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *     General Public License for more details.
+ * "Have I been pwned - importer" is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free  Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
- *     You should have received a copy of the GNU General Public License along
- *     with "Have I been pwned - importer". If not, see <https://www.gnu.org/licenses/>.
+ * "Have I been pwned - importer" is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * "Have I been pwned - importer". If not, see <https://www.gnu.org/licenses/>.
  */
+
 plugins {
     kotlin("jvm") version "1.9.23"
     application
--- src/main/kotlin/net/daester/david/pwned/Main.kt
+++ src/main/kotlin/net/daester/david/pwned/Main.kt
@@ -1,36 +1,32 @@
-/**
- *      This file is part of "Have I been pwned - importer".
+/*
+ * Copyright (c) 2024 David Däster
  *
- *     "Have I been pwned - importer" is free software: you can redistribute
- *     it and/or modify it under the terms of the GNU General Public License
- *     as published by the Free  Software Foundation, either version 3 of the
- *     License, or (at your option) any later version.
+ * This file is part of "Have I been pwned - importer".
  *
- *     "Have I been pwned - importer" is distributed in the hope that it will
- *     be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- *     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *     General Public License for more details.
+ * "Have I been pwned - importer" is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License as
+ * published by the Free  Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
- *     You should have received a copy of the GNU General Public License along
- *     with "Have I been pwned - importer". If not, see <https://www.gnu.org/licenses/>.
+ * "Have I been pwned - importer" is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with "Have I been pwned - importer". If not, see <https://www.gnu.org/licenses/>.
  */
+
 @file:OptIn(ExperimentalCoroutinesApi::class)
 package net.daester.david.pwned
 
-import com.mongodb.client.model.*
-import com.mongodb.client.model.Filters.and
-import com.mongodb.client.model.Filters.eq
 import com.mongodb.kotlin.client.coroutine.MongoClient
-import com.mongodb.kotlin.client.coroutine.MongoCollection
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.flow.collect
 import mu.KLogger
 import mu.KotlinLogging
-import org.bson.BsonDocument
-import org.bson.BsonElement
-import org.bson.BsonInt32
+import net.daester.david.pwned.importer.by_record.ImportByRecord
 import java.nio.file.Path
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
@@ -45,11 +41,10 @@ private val database = System.getenv("MONGODB_DATABASE") ?: "pwnd"
 private val mongoClient: MongoClient = MongoClient.create(mongodbConnectionURL)
 
 private val mongoDB = mongoClient.getDatabase(database)
-private val hashCollection: MongoCollection<HashWithOccurrence> = mongoDB.getCollection<HashWithOccurrence>("hashes")
 
 private val systemProcesses = Runtime.getRuntime().availableProcessors()
 
-private val maxCoroutineFn = systemProcesses * 20
+
 private val logger: KLogger = KotlinLogging.logger {  }
 
 typealias Prefix = String
@@ -67,8 +62,17 @@ data class ChangeObject(
     val toUpdate: List<HashWithOccurrence>,
     val toDelete: List<HashWithOccurrence>
 )
+interface Status {
+    fun increaseQueued()
+    fun increaseReadFiles()
+    fun increaseValidated(increaseBy: Int = 1)
+    fun increaseInserted(increaseBy: Int = 1)
+    fun increaseDeleted(increaseBy: Int = 1)
+    fun increaseObjects(increaseBy: Int = 1)
+    fun increaseUpdated(increaseBy: Int = 1)
+}
 
-private object StatusObject {
+private object StatusObject: Status {
     var fileQueued: Int = 0
         private set
     var filesRead: Int = 0
@@ -84,41 +88,41 @@ private object StatusObject {
         private set
     var deletedCounter: Int = 0
         private set
-    fun increaseQueued() {
+    override fun increaseQueued() {
         synchronized(this) {
             ++fileQueued
         }
     }
-    fun increaseReadFiles() {
+    override fun increaseReadFiles() {
         synchronized(this) {
             ++filesRead
         }
     }
 
-    fun increaseValidated(increaseBy: Int = 1) {
+    override fun increaseValidated(increaseBy: Int) {
         synchronized(this) {
             validatedCounter += increaseBy
         }
     }
 
-    fun increaseUpdated(increaseBy: Int = 1) {
+    override fun increaseUpdated(increaseBy: Int) {
         synchronized(this) {
             updatedCounter += increaseBy
         }
     }
 
-    fun increaseInserted(increaseBy: Int = 1) {
+    override fun increaseInserted(increaseBy: Int) {
         synchronized(this) {
             insertedCounter += increaseBy
         }
     }
 
-    fun increaseDeleted(increaseBy: Int = 1) {
+    override fun increaseDeleted(increaseBy: Int) {
         synchronized(this) {
             deletedCounter += increaseBy
         }
     }
-    fun increaseObjects(increaseBy: Int = 1) {
+    override fun increaseObjects(increaseBy: Int) {
         synchronized(this) {
             objectCounter += increaseBy
         }
@@ -139,38 +143,25 @@ private fun createStatusLogMessage(): String {
     val updated = formatter(StatusObject.updatedCounter)
     val deleted = formatter(StatusObject.deletedCounter)
     return "Queued Files: $queuedFiles - Read files: $readFiles - Processed Objects: $countedObjects - Validated: $validated - Inserted: $inserted - Updated: $updated - Deleted: $deleted"
-
 }
 
-@OptIn(DelicateCoroutinesApi::class)
 fun main() {
     if (path == null) {
         logger.error { "Environment PWNED_PASSWORDS_DIRECTORY is not set. Exit" }
         exitProcess(1)
     }
     runBlocking {
-        val prefixIndex = IndexModel(BsonDocument(listOf(BsonElement(HashWithOccurrence::prefix.name, BsonInt32(1)))))
-        val occurrenceIndex = IndexModel(BsonDocument(listOf(BsonElement(HashWithOccurrence::occurrence.name, BsonInt32(-1)))))
-
-        hashCollection.createIndexes(listOf(prefixIndex, occurrenceIndex)).collect()
         launch(Dispatchers.IO) {
-
+            val ibr = ImportByRecord(status = StatusObject, database = mongoDB)
             val filesToRead = getAllFilePaths(Path.of(path))
-            val dataToProcess = extractFileContent(filesToRead)
-            val entriesToUpsert = calculateNeededUpsertDataObjects(dataToProcess, hashCollection)
-            repeat(maxCoroutineFn) {
-                upsertHashes(entriesToUpsert, hashCollection)
-            }
-            val processStatus = async {
-                while (!entriesToUpsert.isClosedForReceive ) {
-                    logger.info {
-                        createStatusLogMessage()
-                    }
 
-                    delay(1000)
+            val ibrJob = ibr.processHashFiles(filesToRead, this)
+            while (ibrJob.isActive) {
+                logger.info {
+                    createStatusLogMessage()
                 }
+                delay(1000)
             }
-            processStatus.await()
         }
     }
 }
@@ -184,72 +175,6 @@ private fun CoroutineScope.getAllFilePaths(path: Path): ReceiveChannel<Path> =  
     while (files.hasNext()) {
         send(files.next())
         StatusObject.increaseQueued()
-        logger.debug { createStatusLogMessage() }
-    }
-}
-
-private fun CoroutineScope.extractFileContent(fileChannel: ReceiveChannel<Path>): ReceiveChannel<Pair<Prefix, Map<Hash, Int>>> =  produce(
-    capacity = systemProcesses) {
-    for (path in fileChannel) {
-        logger.trace("START: extractFileContent for {}", path.fileName)
-        val prefix = path.fileName.toString().split(".")[0]
-        send(prefix to path.toFile().readLines().associate { line ->
-            val (suffix, amount) = line.split(":")
-            "$prefix$suffix" to amount.toInt(10)
-        })
-        StatusObject.increaseReadFiles()
-        logger.trace("END: extractFileContent for {}", path.fileName)
-
-        logger.debug { createStatusLogMessage() }
-    }
-}
-
-
-private fun CoroutineScope.calculateNeededUpsertDataObjects(
-    fileContent: ReceiveChannel<Pair<Prefix, Map<Hash, Int>>>,
-    dataSourceCollection: MongoCollection<HashWithOccurrence>
-): ReceiveChannel<ChangeObject> = produce(capacity = systemProcesses) {
-    for ((prefix, data ) in fileContent) {
-        logger.trace { "START: calculate upsert for prefix $prefix" }
-        val toDelete = mutableListOf<HashWithOccurrence>()
-        val toUpdate = mutableListOf<HashWithOccurrence>()
-        val toInsert = mutableListOf<HashWithOccurrence>()
-        val visitedHashes = mutableSetOf<Hash>()
-        dataSourceCollection.find(eq(HashWithOccurrence::prefix.name, prefix)).collect {
-            val occurrence = data[it.hash]
-            when {
-                occurrence == null -> if (it.occurrence > 0) toDelete.add(it.copy(occurrence = 0))
-                occurrence != it.occurrence -> toUpdate.add(it.copy(occurrence = occurrence, lastUpdate = LocalDateTime.now()))
-            }
-            visitedHashes.add(it.hash)
-        }
-        data.entries.filterNot { visitedHashes.contains(it.key) }.forEach {
-            toInsert.add(HashWithOccurrence(prefix = prefix, hash = it.key, occurrence = it.value))
-        }
-        if (toDelete.isNotEmpty() || toUpdate.isNotEmpty() || toInsert.isNotEmpty()) {
-            send(ChangeObject(toDelete = toDelete.toList(), toInsert = toInsert.toList(), toUpdate = toUpdate.toList()))
-        }
-        StatusObject.increaseObjects(data.size)
-
-        StatusObject.increaseValidated(data.size - toDelete.size - toUpdate.size - toInsert.size)
-        logger.trace { "END: calculate upsert for prefix $prefix" }
-    }
-}
-
-
-private fun CoroutineScope.upsertHashes(dataObjectsToUpsertInBulk: ReceiveChannel<ChangeObject>, collection: MongoCollection<HashWithOccurrence>) = launch {
-    for (hashesWithOccurrence in dataObjectsToUpsertInBulk) {
-        val toDelete = hashesWithOccurrence.toDelete.map { DeleteOneModel<HashWithOccurrence>(and(eq(HashWithOccurrence::prefix.name, it.prefix), eq(HashWithOccurrence::hash.name, it.hash))) }
-        val toInsert = hashesWithOccurrence.toInsert.map { InsertOneModel(it) }
-        val toUpdate = hashesWithOccurrence.toUpdate.map { UpdateOneModel<HashWithOccurrence>(and(eq(HashWithOccurrence::prefix.name, it.prefix), eq(HashWithOccurrence::hash.name, it.hash)), listOf(Updates.set(HashWithOccurrence::occurrence.name, it.occurrence), Updates.set(HashWithOccurrence::lastUpdate.name, it.lastUpdate))) }
-
-        collection.bulkWrite(
-            toDelete + toInsert + toUpdate,
-            BulkWriteOptions().ordered(false)
-        )
-        StatusObject.increaseDeleted(toDelete.size)
-        StatusObject.increaseInserted(toInsert.size)
-        StatusObject.increaseUpdated(toUpdate.size)
         logger.debug { createStatusLogMessage() }
     }
 }
